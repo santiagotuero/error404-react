e8ef082b95e8c25071f968a4e19b2db0726bc23d
santiagotuero <santiagotuero@gmail.com>
2022-11-05 02:23:09 -0300

    added typography

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Grid, ThemeProvider } from "@mui/material";
 import React from "react";
 
 import Header from "./components/header/Header";
+import { theme } from "./theme";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Grid className="App">
+        <Header />
+      </Grid>
+    </ThemeProvider>
   );
 }
 
